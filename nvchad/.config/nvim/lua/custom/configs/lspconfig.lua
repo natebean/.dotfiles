c06460f58d0f453a8cbb51a6613c3cb9e375515d
Nathan Bean <natebean@pop-os.localdomain>
2023-06-18 13:39:08 -0500

    go formatting

--- nvchad/.config/nvim/lua/custom/configs/lspconfig.lua
+++ nvchad/.config/nvim/lua/custom/configs/lspconfig.lua
@@ -2,7 +2,7 @@ local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "clangd", "tsserver", "gopls" }
+local servers = { "html", "cssls", "clangd", "tsserver" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
@@ -11,56 +11,30 @@ for _, lsp in ipairs(servers) do
   }
 end
 
--- lspconfig.lua_ls.setup({
--- 	on_attach = on_attach,
--- 	capabilities = capabilities,
--- 	settings = {
--- 		Lua = {
--- 			diagnostics = {
--- 				globals = { "vim" },
--- 			},
--- 			workspace = {
--- 				library = {
--- 					[vim.fn.expand("$VIMRUNTIME/lua")] = true,
--- 					[vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
--- 				},
--- 			},
--- 		},
--- 	},
--- })
+local util = require "lspconfig/util"
 
--- local util = require('lspconfig/util')
---
--- local path = util.path
---
--- local function get_python_path(workspace)
---   print("Getting Python Path " .. type(workspace))
---   -- print("Getting Python Path ")
---   -- Use activated virtualenv.
---   if vim.env.VIRTUAL_ENV then
---     -- print("Found VIRTUAL_ENV: " .. vim.env.VIRTUAL_ENV)
---     return path.join(vim.env.VIRTUAL_ENV, 'bin', 'python')
---   end
---
---   -- Find and use virtualenv from pipenv in workspace directory.
---   local match = vim.fn.glob(path.join(workspace, 'Pipfile'))
---   if match ~= '' then
---     local venv = vim.fn.trim(vim.fn.system('PIPENV_PIPFILE=' .. match .. ' pipenv --venv'))
---     return path.join(venv, 'bin', 'python')
---   end
---
---   -- Fallback to system Python.
---   -- print("Using System Python")
---   return vim.fn.exepath('python3') or vim.fn.exepath('python') or 'python'
--- end
+lspconfig.gopls.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  cmd = {"gopls"},
+  filetypes = { "go", "gomod", "gowork", "gotmpl" },
+  root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+  settings = {
+    gopls = {
+      completeUnimported = true,
+      usePlaceholders = true,
+      analyses = {
+        unusedparams = true,
+      },
+    },
+  },
+}
+
 
 lspconfig.pyright.setup {
   on_attach = on_attach,
   capabilities = capabilities,
   filetypes = { "python" },
-  -- on_init = function(client)
-  -- 	client.config.settings.python.pythonPath = get_python_path(client.config.root_dir)
-  -- end,
 }
 
 
--- nvchad/.config/nvim/lua/custom/configs/null-ls.lua
+++ nvchad/.config/nvim/lua/custom/configs/null-ls.lua
@@ -1,18 +1,7 @@
 
--- local null_ls = require("null-ls")
--- null_ls.setup({
--- 	sources = {
--- 		null_ls.builtins.formatting.stylua,
--- 		null_ls.builtins.diagnostics.eslint,
--- 		null_ls.builtins.completion.spell,
--- 		null_ls.builtins.formatting.autopep8,
--- 		null_ls.builtins.diagnostics.flake8,
--- 	},
--- })
-
--- custom/configs/null-ls.lua
 
 local null_ls = require "null-ls"
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 local opts = {
   sources = {
@@ -21,30 +10,27 @@ local opts = {
     null_ls.builtins.formatting.black,
     null_ls.builtins.formatting.stylua,
     null_ls.builtins.formatting.eslint,
-  }
+    null_ls.builtins.formatting.gofumpt,
+    null_ls.builtins.formatting.goimports_reviser,
+    null_ls.builtins.formatting.golines,
+  },
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({
+        group = augroup,
+        buffer = bufnr,
+      })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = bufnr })
+        end,
+      })
+    end
+  end,
+
 }
 
 return opts
-
--- local formatting = null_ls.builtins.formatting
--- local lint = null_ls.builtins.diagnostics
---
--- local sources = {
---    formatting.prettier,
---    formatting.stylua,
--- 	 formatting.eslint,
--- 	 formatting.autopep8,
---
--- 	 lint.flake8,
---    lint.shellcheck,
---
--- 	 null_ls.builtins.completion.spell,
--- }
---
--- null_ls.setup {
---    debug = true,
---    sources = sources,
--- }
-
-
 
