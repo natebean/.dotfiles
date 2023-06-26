a22237c2e7de7d5f10dae1a10c4d902aaab221c8
Nathan Bean <natebean@pop-os.localdomain>
2023-06-25 20:17:10 -0500

    clean up

--- nvchad/.config/nvim/lua/custom/plugins.lua
+++ nvchad/.config/nvim/lua/custom/plugins.lua
@@ -1,72 +1,65 @@
 local plugins = {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = {
-        -- defaults
-        "vim",
-        "lua",
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = {
+			ensure_installed = {
+				-- defaults
+				"vim",
+				"lua",
 
-        -- web dev
-        "html",
-        "css",
-        "javascript",
-        "typescript",
-        "tsx",
-        "json",
+				-- web dev
+				"html",
+				"css",
+				"javascript",
+				"typescript",
+				"tsx",
+				"json",
 
-        -- low level
-        "go",
-        "python",
-        "rust",
-      },
-    },
-  },
-  -- In order to modify the `lspconfig` configuration:
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require "plugins.configs.lspconfig"
-      require "custom.configs.lspconfig"
-    end,
-  },
+				-- low level
+				"go",
+				"python",
+				"rust",
+			},
+		},
+	},
+	-- In order to modify the `lspconfig` configuration:
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			require("plugins.configs.lspconfig")
+			require("custom.configs.lspconfig")
+		end,
+	},
 
-  {
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "lua-language-server",
-        "html-lsp",
-        "pyright",
-        "rust-analyzer",
-        "gopls",
-        "prettier",
-        "stylua",
-        "mypy",
-        "ruff",
-        "black",
-      },
-    },
-    automatic_installation = true,
-  },
+	{
+		"williamboman/mason.nvim",
+		opts = {
+			ensure_installed = {
+				"lua-language-server",
+				"html-lsp",
+				"pyright",
+				"rust-analyzer",
+				"gopls",
+				"prettier",
+				"stylua",
+				"mypy",
+				"ruff",
+				"black",
+			},
+		},
+		automatic_installation = true,
+	},
 
-  {
-    "neovim/nvim-lspconfig",
+	{
+		"neovim/nvim-lspconfig",
 
-    dependencies = {
-      "jose-elias-alvarez/null-ls.nvim",
-      ft = { "python", "go", "lua" },
-      -- config = function()
-      --   require "custom.configs.null-ls"
-      -- end,
-      opts = function()
-        return require "custom.configs.null-ls"
-      end,
-    },
-    -- config = function()
-    --   require "plugins.configs.lspconfig"
-    --   require "custom.configs.lspconfig"
-    -- end,
-  },
+		dependencies = {
+			"jose-elias-alvarez/null-ls.nvim",
+			ft = { "python", "go", "lua" },
+			opts = function()
+				return require("custom.configs.null-ls")
+			end,
+		},
+	},
 }
 return plugins
