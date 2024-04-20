d781a041d2b7be3ef0e8d73435618657228441dd
Nathan Bean <natebean@pop-os.localdomain>
2024-04-20 08:07:53 -0500

    remove nvchad

--- nvchad/.config/nvim/lua/custom/init.lua
+++
@@ -1,8 +0,0 @@
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.mousemodel = "extend" -- no popup
--- vim.opt.wrap = false
-vim.opt.colorcolumn = 80
-vim.cmd("let g:sneak#label = 1")
-
-vim.cmd([[autocmd BufWritePre <buffer> lua vim.lsp.buf.format()]])
